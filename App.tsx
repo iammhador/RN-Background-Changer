3c81cbf6a10ad7cc3dbb4638bb7a8d8424f96415
iammhador <mhador123@gmail.com>
2024-02-27 03:46:53 +0600

    Add: Loop and collect random color

--- App.tsx
+++ App.tsx
@@ -1,14 +1,59 @@
-import {Text, View} from 'react-native';
-import React from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
 
 const App = () => {
+  const [randomColor, setRandomColor] = useState<string>('#ffffff');
+
+  const generateRandomColor = () => {
+    const hexNumber = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let color = '#';
+    for (let index = 0; index < 6; index++) {
+      color += hexNumber[Math.floor(Math.random() * 16)];
+    }
+
+    setRandomColor(color);
+  };
+
   return (
-    <View>
-      <Text>Hello World Color Changer</Text>
-    </View>
+    <>
+      <StatusBar backgroundColor={'#BED1CF'} />
+      <View style={[styles.container, {backgroundColor: randomColor}]}>
+        <TouchableOpacity onPress={generateRandomColor}>
+          <View style={styles.PrimaryButton}>
+            <Text style={styles.PrimaryButtonText}>Press Me</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 };
 
 export default App;
 
-// const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  PrimaryButton: {
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    borderRadius: 5,
+    backgroundColor: '#BED1CF',
+  },
+  PrimaryButtonText: {
+    fontSize: 16,
+    fontWeight: '700',
+    fontStyle: 'normal',
+    textTransform: 'uppercase',
+    color: '#E78895',
+  },
+});
